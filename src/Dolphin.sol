0e84dbb8ddccdc2812acdacf921f2af7dff231d6
ralphchris <ralphel2006@gmail.com>
2026-07-03 19:19:19 +0100

    forge fmt

--- src/Dolphin.sol
+++ src/Dolphin.sol
@@ -4,17 +4,15 @@ pragma solidity ^0.8.24;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dolphin is ERC721 {
-
     uint256 private s_tokenCounter;
     string private s_dolphinUri;
 
-    mapping(uint256 s_tokenCounter => string s_dolphinUri)  private tokenCounterToUri;
+    mapping(uint256 s_tokenCounter => string s_dolphinUri) private tokenCounterToUri;
 
     constructor(string memory dolphinUri) ERC721("Dolphin", "DLP") {
         s_dolphinUri = dolphinUri;
         s_tokenCounter = 0;
-
-    } 
+    }
 
     function safeMint() public {
         tokenCounterToUri[s_tokenCounter] = s_dolphinUri;
@@ -22,7 +20,7 @@ contract Dolphin is ERC721 {
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns(string memory dolphinUri) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory dolphinUri) {
         return tokenCounterToUri[tokenId];
     }
 }
